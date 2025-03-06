844c5cea0ac2705303b60463b4f8df6c4445adf8
khangtran2020 <kt36@njit.edu>
2025-03-06 16:41:50 -0500

    debug get_propmt

--- data/core.py
+++ data/core.py
@@ -474,6 +474,7 @@ class Data(object):
         self.logger.log("[green]Data is ready![/green]")
         self.logger.log(f"Size of data data: {len(self.processed_data)}")
         if self.debug:
+            self.logger.log(f"Branch line: {branch_line}")
             self.logger.log(f"Sample prompt: {full_text}")
         quartiles = np.quantile(num_tokens, [0, 0.25, 0.5, 0.75, 1])
         self.logger.log(f"Statistics of # tokens: {quartiles}")
