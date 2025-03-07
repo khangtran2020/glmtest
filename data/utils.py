6d6d8459e400bafbd0795b9c8916e150dd62ec71
khangtran2020 <kt36@njit.edu>
2025-03-07 17:44:35 -0500

    debugging

--- data/utils.py
+++ data/utils.py
@@ -73,6 +73,7 @@ def get_dataset(
             tokenizer=tokenizer,
             llm_tokenizer=llm_tokenizer,
             graph=graph,
+            baseline_prompt=baseline_prompt,
             debug=debug,
         )
     elif data_name == "testgeneval":
@@ -84,6 +85,7 @@ def get_dataset(
             model=model,
             tokenizer=tokenizer,
             llm_tokenizer=llm_tokenizer,
+            baseline_prompt=baseline_prompt.
             debug=debug,
         )
     else:
