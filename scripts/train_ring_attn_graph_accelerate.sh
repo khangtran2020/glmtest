9c96a4e5a606a592a2948a3d17ca68cc852f701e
khangtran2020 <kt36@njit.edu>
2025-03-19 11:33:11 -0400

    add accelerate multi gpus

---
+++ scripts/train_ring_attn_graph_accelerate.sh
@@ -0,0 +1,20 @@
+accelerate launch main.py --mode train \
+    --seed 42 \
+    --data_path Dataset \
+    --data testgeneval \
+    --baseline_prompt graph \
+    --llm_model "Qwen/CodeQwen1.5-7B-Chat" \
+    --max_seq_len 16384 \
+    --batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --num_gpu 2 \
+    --output_dir "./results/models/" \
+    --overwrite_output_dir \
+    --do_train \
+    --do_eval \
+    --learning_rate 5e-5 \
+    --max_grad_norm 1.0 \
+    --num_train_epochs 1 \
+    --dtype bfloat16 \
+    --debug \
+    --use_lora
