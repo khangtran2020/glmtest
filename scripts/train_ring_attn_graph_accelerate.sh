ed40a6d3a51d5abbc87b996237b68670f5b37245
khangtran2020 <kt36@njit.edu>
2025-03-21 12:29:11 -0400

    modifying scripts

--- scripts/train_ring_attn_graph_accelerate.sh
+++ scripts/train_ring_attn_graph_accelerate.sh
@@ -1,4 +1,4 @@
-accelerate launch main.py --mode train \
+accelerate launch --num_processes 2 main.py --mode train \
     --seed 42 \
     --data_path Dataset \
     --data testgeneval \
@@ -7,7 +7,7 @@ accelerate launch main.py --mode train \
     --max_seq_len 16384 \
     --batch_size 1 \
     --gradient_accumulation_steps 1 \
-    --num_gpu 4 \
+    --num_gpu 2 \
     --output_dir "./results/models/" \
     --overwrite_output_dir \
     --do_train \
