505d772456479c7e31ac2a7f072b5ba3b8006b05
khangtran2020 <kt36@njit.edu>
2025-04-21 09:26:23 -0400

    changing config

--- train/train.py
+++ train/train.py
@@ -828,14 +828,13 @@ def train_single_gpu_accelerate(
 
     final_model_path = os.path.join(save_path, "final_model")
     console.log(f"Saving final model to {final_model_path}...")
-
-    # unwrapped_model.save_pretrained(
-    #     final_model_path,
-    #     is_main_process=accelerator.is_main_process,
-    #     save_function=accelerator.save,
-    # )
     final_tokenizer_path = os.path.join(save_path, "tokenizer")
-    unwrapped_model.save_pretrained(final_model_path)
+
+    unwrapped_model.save_pretrained(
+        final_model_path,
+        is_main_process=accelerator.is_main_process,
+        save_function=accelerator.save,
+    )
     tokenizer.save_pretrained(final_tokenizer_path)
 
     console.log(f"Final model saved to {final_model_path}")
@@ -1156,14 +1155,13 @@ def train_multi_gpu_accelerate(
 
         final_model_path = os.path.join(save_path, "final_model.pt")
         console.log(f"Saving final model to {final_model_path}...")
-
-        # unwrapped_model.save_pretrained(
-        #     final_model_path,
-        #     is_main_process=accelerator.is_main_process,
-        #     save_function=accelerator.save,
-        # )
         final_tokenizer_path = os.path.join(save_path, "tokenizer")
-        unwrapped_model.save_pretrained(final_model_path)
+
+        unwrapped_model.save_pretrained(
+            final_model_path,
+            is_main_process=accelerator.is_main_process,
+            save_function=accelerator.save,
+        )
         tokenizer.save_pretrained(final_tokenizer_path)
 
         console.log(f"Final model saved to {final_model_path}")
