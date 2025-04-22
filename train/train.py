ec59c03fe311ba8dec7d6bb8e48cbaa6af3c03f7
khangtran2020 <kt36@njit.edu>
2025-04-21 20:16:55 -0400

    changing config

--- train/train.py
+++ train/train.py
@@ -833,6 +833,8 @@ def train_single_gpu_accelerate(
     console.log(f"Saving final model to {final_model_path}...")
     final_tokenizer_path = os.path.join(save_path, "tokenizer")
 
+    console.log(f"Config: {unwrapped_model.config}")
+
     unwrapped_model.save_pretrained(
         final_model_path,
         is_main_process=accelerator.is_main_process,
