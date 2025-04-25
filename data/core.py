fd92eb0f8fac0d470f604619f4cf558b92063438
khangtran2020 <kt36@njit.edu>
2025-04-24 21:33:08 -0400

    add graph stats

--- data/core.py
+++ data/core.py
@@ -850,7 +850,7 @@ class Data(object):
         # radius = nx.radius(nx_graph)
         # print("Radius of the graph:", radius)
 
-        seeds = mask[0]
+        seeds = torch.Tensor([10])
         blocks = []
         print(
             f"Sampling neighbors for {seeds.size()} nodes, from graph with {graph.num_nodes()} nodes"
