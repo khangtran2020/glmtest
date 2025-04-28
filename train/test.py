bd95837b1f9a5de5f470584a84d194491eecd104
khangtran2020 <kt36@njit.edu>
2025-04-28 09:53:13 -0400

    fixing device problem in testing

--- train/test.py
+++ train/test.py
@@ -25,6 +25,7 @@ def test(
     collate_fn: callable = collate_fn,
 ):
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    console.log("Testing on device ... :", device)
     te_dataset = GLMFDataset(
         data=dataset.test_data,
         tokenizer=dataset.llm_tokenizer,
