5fb2f96ab6ca66c19d376de3b40cf75bd9c7813c
khangtran2020 <kt36@njit.edu>
2025-05-07 14:09:25 -0400

    add debug

--- train/train.py
+++ train/train.py
@@ -417,12 +417,20 @@ def train_multi_gpu_accelerate(
     accelerator.print(f"Mixed precision: {mixed_precision}")
 
     if args.model_debug == False:
-        tr_dataset = GLMFDataset(
-            data=dataset.train_data,
-            tokenizer=dataset.llm_tokenizer,
-            max_seq_length=args.max_seq_length,
-            debug=args.debug,
-        )
+        if args.debug:
+            tr_dataset = GLMFDataset(
+                data=dataset.train_data[600:],
+                tokenizer=dataset.llm_tokenizer,
+                max_seq_length=args.max_seq_length,
+                debug=args.debug,
+            )
+        else:
+            tr_dataset = GLMFDataset(
+                data=dataset.train_data,
+                tokenizer=dataset.llm_tokenizer,
+                max_seq_length=args.max_seq_length,
+                debug=args.debug,
+            )
         va_dataset = GLMFDataset(
             data=dataset.val_data,
             tokenizer=dataset.llm_tokenizer,
