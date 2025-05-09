5f7bdc62c45107114747fdb3020f6fdd1e7ff3d3
khangtran2020 <kt36@njit.edu>
2025-05-09 14:58:44 -0400

    fix prompt template

--- data/core.py
+++ data/core.py
@@ -771,7 +771,10 @@ class Data(object):
         # if len(task_prompt) > self.max_tokens:
         #     task_prompt = task_prompt[: self.max_tokens]
 
-        return text, response, task_prompt
+        assert task_prompt_input in task_prompt
+        task_prompt_input = task_prompt.replace(task_prompt_input, "")
+
+        return task_prompt_input, response, task_prompt
 
     def add_fuzz_tags(self, code: str, tag: str = "fuzz") -> str:
         try:
