e38f1e3eea2c80ebed3af7269ba299078cc5e1fb
khangtran2020 <kt36@njit.edu>
2025-07-06 19:51:51 -0400

    debugging

--- train/train.py
+++ train/train.py
@@ -640,9 +640,7 @@ def train_multi_gpu_accelerate(
                             grad_norm = 0.0
                             for name, param in model.named_parameters():
                                 if param.grad is not None:
-                                    grad_norm = (
-                                        grad_norm + param.grad.values().norm(2) ** 2
-                                    )
+                                    grad_norm = grad_norm + param.grad.norm(2) ** 2
                             grad_norm = grad_norm.sqrt().item()
                             console.log(
                                 f"Step {global_step} - for rank {local_rank}: gradient norm: {grad_norm:.4f}"
