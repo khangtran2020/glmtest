ced74d1a3d2dd6fb4678e4f8877b00222ab0a8fc
khangtran2020 <kt36@njit.edu>
2025-07-20 01:11:38 -0400

    add fuzz mask

--- model/model.py
+++ model/model.py
@@ -930,22 +930,21 @@ class GLMFModelFuzzing(GLMFModel, GenerationMixin):
             )
         else:
             logits = self.llm_model.lm_head(hidden_states[:, slice_indices, :])
-        loss = None
 
-        pprint(
-            f"[yellow]Step {step}[/yellow]: [cyan]logits shape: {logits.shape}[/cyan]"
-        )
+        loss = None
         if labels is not None:
-            logits = logits.float()
-            logits = logits.view(-1, self.config.vocab_size)
-            # labels = labels.view(-1)
-            labels = labels.to(logits.device)
-            loss = fixed_cross_entropy(
-                logits,
-                labels,
-                num_items_in_batch=None,
-                ignore_index=-100,
-            )
+            if hasattr(self.llm_model, "base_model"):
+                self.llm_model.base_model.loss_function(
+                    logits=logits,
+                    labels=labels,
+                    vocab_size=self.config.vocab_size,
+                )
+            else:
+                self.llm_model.loss_function(
+                    logits=logits,
+                    labels=labels,
+                    vocab_size=self.config.vocab_size,
+                )
             loss = loss + self.kl_g_reg * kl_g_total + self.kl_d_reg * kl_d_total
 
         return CausalLMOutputWithPast(
