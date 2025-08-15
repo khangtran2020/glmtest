da12e94ae85097a3ed1f359a3f87b939d004fb1a
khangtran2020 <kt36@njit.edu>
2025-08-15 01:50:08 -0400

    debugging

--- model/model.py
+++ model/model.py
@@ -537,9 +537,6 @@ class GLMFModelForCausalLM(GLMFModel, GenerationMixin):
             inputs_embeds=inputs_embeds,
             labels=labels,
             use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
             cache_position=cache_position,
         )
 
