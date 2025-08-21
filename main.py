1544fc519760b838d07425992b9df0cc18d89b02
khangtran2020 <kt36@njit.edu>
2025-08-21 00:38:10 -0400

    debugging testing

--- main.py
+++ main.py
@@ -449,6 +449,7 @@ def main() -> None:
                 if use_lora:
                     model.llm_model = model.llm_model.merge_and_unload()
 
+        model = model.to(dtype=torch.bfloat16)
         console.log(f"Model is loaded to device: {model.device}")
         test(args=args, dataset=dataset, model=model, console=console)
 
