4988d80cc93789afb34cb26f1406dd6ffe4809a5
khangtran2020 <kt36@njit.edu>
2025-08-24 11:12:25 -0400

    debugging

--- main.py
+++ main.py
@@ -448,6 +448,7 @@ def main() -> None:
                 model.load_state_dict(state_dict)
                 if use_lora:
                     model.llm_model = model.llm_model.merge_and_unload()
+                console.log(f"Model weights loaded from {file}")
 
         # model = model.to(dtype=torch.float)
         # unifying dtype to avoid errors
