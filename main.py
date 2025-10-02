fc46c03829bc2c1da8807780e91fdc70255290e6
khangtran2020 <kt36@njit.edu>
2025-10-02 18:16:48 -0400

    debugging testgen pipeline

--- main.py
+++ main.py
@@ -672,10 +672,14 @@ def main() -> None:
         for n, p in model.named_parameters():
             if args.dtype == "bf16":
                 if p.dtype != torch.bfloat16:
-                    p.data = p.data.to(torch.bfloat16, device=device)
+                    p.data = p.data.to(torch.bfloat16)
+                if p.device != device:
+                    p.data = p.data.to(device)
             elif args.dtype == "fp16":
                 if p.dtype != torch.float16:
-                    p.data = p.data.to(torch.float16, device=device)
+                    p.data = p.data.to(torch.float16)
+                if p.device != device:
+                    p.data = p.data.to(device)
 
         console.log(
             f"Model is loaded to device: {model.device} - with type {model.dtype}"
