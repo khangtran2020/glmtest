a177dd2a074b17e13eb73caed5108a8063b9af84
khangtran2020 <kt36@njit.edu>
2025-10-02 17:28:56 -0400

    debugging testgen pipeline

--- model/model.py
+++ model/model.py
@@ -316,7 +316,9 @@ class GLMFModelForCausalLM(GLMFModel, GenerationMixin):
             batch_size = inputs_embeds.size(0)
 
             for i in range(batch_size):
-                graph = graphs[i].to(self.llm_model.device)
+                graph = graphs[i]
+                for key in graph.keys():
+                    graph[key] = graph[key].to(self.llm_model.device)
                 graph_mask = graph_masks[i].to(self.llm_model.device)
                 graph_embeds = self.gnn(graph, graph_mask)
                 graph_embeds = graph_embeds.to(inputs_embeds.device)
@@ -996,6 +998,8 @@ class GLMFModelFuzzing(GLMFModel, GenerationMixin):
 
             for i in range(batch_size):
                 graph = graphs[i]
+                for key in graph.keys():
+                    graph[key] = graph[key].to(self.llm_model.device)
                 graph_mask = graph_masks[i].to(self.llm_model.device)
                 graph_embeds = self.gnn(graph, graph_mask)
                 graph_embeds = graph_embeds.to(inputs_embeds.device)
