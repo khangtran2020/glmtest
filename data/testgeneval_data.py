f34b385e9078738438d9db0e784ea86f71b72d37
khangtran2020 <kt36@njit.edu>
2025-10-22 20:34:07 -0400

    add new data pipeline

--- data/testgeneval_data.py
+++ data/testgeneval_data.py
@@ -148,7 +148,7 @@ class TestGenEval(Data):
 
             for i, task in enumerate(raw_data):
                 if NEW_KEY_ID not in task.keys():
-                    task[NEW_KEY_ID] = i
+                    task[NEW_KEY_ID] = task["instance_id"]
 
             raw_data = {task[NEW_KEY_ID]: task for task in raw_data}
 
