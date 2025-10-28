3fe1522f91d4191d0b6802363ff1fa1264fdddf5
khangtran2020 <kt36@njit.edu>
2025-10-28 19:19:10 -0400

    remove sampling

--- inference/test.py
+++ inference/test.py
@@ -283,8 +283,8 @@ def generate_and_save_on_one_dataset(
                     top_k=40,
                     no_repeat_ngram_size=3,
                     max_new_tokens=args.max_new_tokens,
-                    do_sample=True,
-                    repetition_penalty=1.1,
+                    do_sample=False,
+                    repetition_penalty=1.5,
                 )
 
                 with torch.autocast(device_type="cuda", dtype=torch.float16):
