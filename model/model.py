7fac79faa492e1aa076f5626addc10211f638306
khangtran2020 <kt36@njit.edu>
2025-11-01 11:40:06 -0400

    debugging

--- model/model.py
+++ model/model.py
@@ -344,7 +344,9 @@ def get_model_test(
 
     console.log(f"Model is loaded to device: {model.device} - with type {model.dtype}")
     for name, param in model.named_parameters():
-        console.log(f"[yellow]Parameter {name}, dtype: {param.dtype}[/yellow]")
+        console.log(
+            f"[yellow]Parameter {name}, dtype: {param.dtype}, devices: {param.device}[/yellow]"
+        )
     return model
 
 
