44f6d7356c8ee51d43df93cfc24aee65be9bd0ba
khangtran2020 <kt36@njit.edu>
2025-11-02 16:44:04 -0500

    add save and load from best model

--- model/model.py
+++ model/model.py
@@ -336,12 +336,9 @@ def get_model_test(
                 map_location=f"cuda:{rank}" if args.num_gpu > 1 else "cpu",
             )
             console.log(f"[green]Using LoRA weights for testing: {use_lora}.[/green]")
-            for key in list(state_dict.keys()):
-                if "base_model." in key:
-                    new_key = key.replace(".base_model.model", "").replace(
-                        ".base_layer", ""
-                    )
-                    state_dict[new_key] = state_dict.pop(key)
+            # for key in list(state_dict.keys()):
+            if "current_checkpoint" in args.model_weight_path:
+                state_dict = state_dict["model_state_dict"]
             model.load_state_dict(state_dict)
             if use_lora:
                 model.llm_model = model.llm_model.merge_and_unload()
