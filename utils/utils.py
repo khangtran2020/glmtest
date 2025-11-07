ac5e605666b3b3689415faee9ff6413e5a32d4f6
khangtran2020 <kt36@njit.edu>
2025-11-07 12:54:12 -0500

    removing weight only == True

--- utils/utils.py
+++ utils/utils.py
@@ -349,6 +349,7 @@ def extract_list_content(input_string) -> List[str]:
 def get_index_by_value(a, val) -> torch.Tensor:
     if isinstance(a, np.ndarray):
         a = torch.from_numpy(a)
+    assert isinstance(a, torch.Tensor)
     return (a == val).nonzero(as_tuple=True)[0]
 
 
