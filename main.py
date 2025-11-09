1f07cf3aa8cabfe905c982fb82012eacc862964b
khangtran2020 <kt36@njit.edu>
2025-11-09 12:18:44 -0500

    Add prompt deduce

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def main() -> None:
             if args.module_path is None:
                 dataset.prepare_data_for_test_gen()
         else:
-            dataset.prepare_data()
+            dataset.prepare_data(old_data_path=args.old_data_path)
 
     if args.mode == "baseline":
         if args.baseline_prompt_type == "prompt_engineer":
