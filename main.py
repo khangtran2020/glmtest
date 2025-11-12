093b6721d7d74637c948ae4c24c74a8a4157c0c6
khangtran2020 <kt36@njit.edu>
2025-11-11 21:58:34 -0500

    change val size

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ def main() -> None:
         dataset.prepare_data_by_repo()
 
     dataset.train_test_split(
-        val_split=int(100), test_only=True if args.mode == "testgen" else False
+        val_split=int(1000), test_only=True if args.mode == "testgen" else False
     )
 
     console.log(f"Broadcasted args and dataset to all processes.")
