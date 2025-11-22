551c875e46122069c906e2401cd0d5326a1abdc4
khangtran2020 <kt36@njit.edu>
2025-11-21 20:04:01 -0500

    debugging

--- config.py
+++ config.py
@@ -170,7 +170,7 @@ def add_model_group(group):
         "--max_num_checkpoint",
         type=int,
         help="maximum number of checkpoints",
-        default=2,
+        default=5,
     )
     group.add_argument(
         "--model_weight_path", type=str, help="path to the model weight", default=None
