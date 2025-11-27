24b12ea3b2b0029bcb7f4d74192a20e762fec0dd
khangtran2020 <kt36@njit.edu>
2025-11-27 18:35:22 -0500

    debugging

--- train/train.py
+++ train/train.py
@@ -1124,7 +1124,7 @@ def train_multi_gpu_accelerate(
         "batch_size": args.batch_size,
         "collate_fn": collate_fn,
         "sampler": sampler,
-        "num_workers": 1,  # Use os.cpu_count() workers
+        "num_workers": 0,  # Use os.cpu_count() workers
         "pin_memory": True,
         "persistent_workers": True,
     }
