0a321710c1fe764b861ca08bdd24122b7ed9ad1b
khangtran2020 <kt36@njit.edu>
2025-12-12 12:09:36 -0500

    debugging

--- model/gnn.py
+++ model/gnn.py
@@ -3,6 +3,7 @@
 import torch.nn
 from torch import nn
 from torch_geometric.nn import SAGEConv, GATConv
+from typing import Dict
 
 
 class GAT(torch.nn.Module):
@@ -40,9 +41,11 @@ class GAT(torch.nn.Module):
             )
         self.activation = torch.nn.SELU()
 
-    def forward(self, x: torch.Tensor, edge_index: torch.Tensor):
+    def forward(self, x: Dict[torch.Tensor], edge_index: torch.Tensor):
         h = x
         for i in range(0, self.n_layers):
+            if len(h.keys()) == 1:
+                h = (h["node"], h["node"])
             h = self.layers[i](h, edge_index)
             h = self.activation(h)
         h = self.last_layer(h)
