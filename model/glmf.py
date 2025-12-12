3255550e760649e26a71df66a561efcbe57f08df
khangtran2020 <kt36@njit.edu>
2025-12-12 13:42:55 -0500

    debugging

--- model/glmf.py
+++ model/glmf.py
@@ -344,13 +344,17 @@ class GLMFModelForCausalLM(GLMFModel, GenerationMixin):
                 overall_mask = overall_mask.long().to(self.llm_model.device)
                 # assert isinstance(self.gnn,)
                 # print("Inputs emebdding:", graph.x_dict)
-                for key in graph.x_dict:
-                    print(f"Graph node type: {key}, dtype: {graph.x_dict[key].dtype}")
+                # for key in graph.x_dict:
+                #     print(f"Graph node type: {key}, dtype: {graph.x_dict[key].dtype}")
 
-                for key in graph.edge_index_dict:
-                    print(
-                        f"Graph edge type: {key}, dtype: {graph.edge_index_dict[key].dtype}"
-                    )
+                # for key in graph.edge_index_dict:
+                #     print(
+                #         f"Graph edge type: {key}, dtype: {graph.edge_index_dict[key].dtype}"
+                #     )
+                target_dtype = next(self.gnn.parameters()).dtype
+                for k, v in graph.x_dict.items():
+                    graph.x_dict[k] = v.to(dtype=target_dtype)
+
                 graph_embeds = self.gnn(graph.x_dict, graph.edge_index_dict)
 
                 node_idx = self.get_index_by_value(overall_mask, 1)
