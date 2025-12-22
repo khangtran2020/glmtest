cf9e41797dbb099a234203a6b5680038367faa08
khangtran2020 <kt36@njit.edu>
2025-12-22 16:38:05 -0500

    debugging

--- branch/utils.py
+++ branch/utils.py
@@ -353,8 +353,8 @@ def get_all_branch(
         set_of_end = [
             e
             for e in set_of_endline
-            if e > line_dict["functions"][func_name][0]
-            and e <= line_dict["functions"][func_name][1]
+            if e > line_dict["async_functions"][func_name][0]
+            and e <= line_dict["async_functions"][func_name][1]
         ]
         if len(set_of_end) == 0:
             continue
